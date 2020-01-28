424108e1e41d998b813aea75f672ce20a758d90f
Erick Nicolas Nunes da Hora <erick.hora@ufpe.br>
2020-01-28 15:47:05 -0300

    - Instalado axios.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import axios from './plugins/axios'
 import '@babel/polyfill'
 
 Vue.config.productionTip = false
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
+  axios,
   render: h => h(App)
 }).$mount('#app')
